eb9f31a70926b18eb145333bcfa082d0514c26b9
youn1223 <youn1223@daum.net>
2025-03-19 13:47:18 +0900

    youn1223 1:47

--- 13_MyBatis_Album/src/main/java/album/controller/AlbumDeleteController.java
+++ 13_MyBatis_Album/src/main/java/album/controller/AlbumDeleteController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import album.model.AlbumDao;
-
+ 
 @Controller
 public class AlbumDeleteController {
 
